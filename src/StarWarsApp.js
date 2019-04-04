853fb25e99be5a2375066f8573b74c416312ffc0
ut_mat <ut_mat@A33COMP72>
2019-04-04 19:30:30 +0300

    second commit

---
+++ src/StarWarsApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class StartWars extends Component {
+  render() {
+    return (
+        <div>
+            <h2>StartWars component works!</h2>
+        </div>
+    );
+  }
+}
